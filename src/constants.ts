869b563af5c0b47b6ef3f466cfc1dd92644d2e85
ThapaSurace <zorosenpai456@gmail.com>
2024-08-30 14:17:26 +0545

    add sample data for cart items

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const cartItems = [
+	{
+		id: 1,
+		name: 'Red Hudy',
+		image: '/redhudy.jpeg',
+		price: 60.0,
+		quantity: 5,
+	},
+	{
+		id: 2,
+		name: 'Black Trouser',
+		image: '/blacktrouser.jpeg',
+		price: 20,
+		quantity: 6,
+	},
+];
