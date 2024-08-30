ccd2d0e8f8f520ad8f938f91603f66488c66e934
ThapaSurace <zorosenpai456@gmail.com>
2024-08-30 14:20:02 +0545

    feat(cart): add CartList component with item display

---
+++ src/components/cart/CartList.tsx
@@ -0,0 +1,59 @@
+import { cartItems } from "@/constants";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Plus, Minus } from "lucide-react";
+import { Button } from "../ui/button";
+
+const CartList = () => {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="">Item</TableHead>
+          <TableHead>Quantity</TableHead>
+          <TableHead className="text-right">Total</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {cartItems.map((item) => (
+          <TableRow key={item.id}>
+            <TableCell className="font-medium">
+              <div className="flex items-center space-x-4">
+                <img
+                  src={item.image}
+                  alt={item.name}
+                  className="w-32 h-32 object-cover rounded-lg"
+                />
+                <div>
+                  <div className="font-semibold text-gray-900">{item.name}</div>
+                  <div className="text-sm text-gray-500">${item.price}</div>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell>
+              <div className="flex gap-2 items-center">
+                <Button variant="outline" size="icon">
+                  <Plus className="h-4 w-4" />
+                </Button>
+                <span>{item.quantity}</span>
+                <Button variant="outline" size="icon">
+                  <Minus className="h-4 w-4" />
+                </Button>
+              </div>
+            </TableCell>
+            <TableCell className="text-right">
+              ${item.price * item.quantity}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
+export default CartList;
