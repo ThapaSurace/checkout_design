15bc0c735c38c7b2ab46c55f75bb4d85ab046955
ThapaSurace <zorosenpai456@gmail.com>
2024-08-30 14:48:31 +0545

    add ordersummarycard component to cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,22 @@
 import CartList from "@/components/cart/CartList";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import OrderSummaryCard from "@/components/OrderSummaryCard";
 
 const Cart = () => {
   return (
-    <MaxWidthWrapper>
-      <CartList />
+    <MaxWidthWrapper className=" space-y-6  md:flex md:gap-10 my-16">
+      <div className="basis-[70%]">
+        <h1 className=" text-2xl font-semibold tracking-tight mb-6 text-secondary-foreground">
+          Cart Items
+        </h1>
+        <CartList />
+      </div>
+      <div className="basis-[30%]">
+        <h1 className=" text-2xl font-semibold tracking-tight mb-6 text-secondary-foreground">
+          Order Summary
+        </h1>
+        <OrderSummaryCard />
+      </div>
     </MaxWidthWrapper>
   );
 };
