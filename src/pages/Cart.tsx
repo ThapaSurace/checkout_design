a155337ee6a17c482b392f4ce217740499532daa
ThapaSurace <zorosenpai456@gmail.com>
2024-08-30 19:37:37 +0545

    add components form shadcn

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,7 +4,7 @@ import OrderSummaryCard from "@/components/OrderSummaryCard";
 
 const Cart = () => {
   return (
-    <MaxWidthWrapper className=" space-y-6  md:flex md:gap-10 my-16">
+    <MaxWidthWrapper className=" space-y-6  md:flex md:gap-16 my-16">
       <div className="basis-[70%]">
         <h1 className=" text-2xl font-semibold tracking-tight mb-6 text-secondary-foreground">
           Cart Items
