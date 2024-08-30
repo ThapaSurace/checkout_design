5f552130fcc586415d89c7d7c139f7ed1232ccba
ThapaSurace <zorosenpai456@gmail.com>
2024-08-30 14:20:35 +0545

    feat(cart): add Cart page with CartList and MaxWidthWrapper

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,11 @@
+import CartList from "@/components/cart/CartList";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+const Cart = () => {
+  return (
+    <MaxWidthWrapper>
+      <CartList />
+    </MaxWidthWrapper>
+  );
+};
+export default Cart;
