058b4d8f4464015022bc755390e298ab204a95b5
ThapaSurace <zorosenpai456@gmail.com>
2024-08-31 12:51:44 +0545

    update imports

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import CartList from "@/components/cart/CartList";
+import CartList from "@/components/CartList";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import OrderSummaryCard from "@/components/OrderSummaryCard";
 
