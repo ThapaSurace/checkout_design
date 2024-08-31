db2ab2bb196c6faa62863dd93dd0dd6da240332b
ThapaSurace <zorosenpai456@gmail.com>
2024-08-31 16:02:47 +0545

    implement subtotal component

--- src/components/OrderSummaryCard.tsx
+++ src/components/OrderSummaryCard.tsx
@@ -1,31 +1,13 @@
 import { Link } from "react-router-dom";
 import { Button } from "./ui/button";
 import { Card, CardContent, CardFooter } from "./ui/card";
-import { Separator } from "./ui/separator";
+import SubTotal from "./SubTotal";
 
 const OrderSummaryCard = () => {
   return (
     <Card className="pt-4">
       <CardContent className="space-y-6">
-        <div className="space-y-4">
-          <div className=" text-muted-foreground flex justify-between">
-            <span>Subtotal</span>
-            <span>$100</span>
-          </div>
-          <div className=" text-muted-foreground flex justify-between">
-            <span>Shipping</span>
-            <span>$0</span>
-          </div>
-          <div className=" text-muted-foreground flex justify-between">
-            <span>Taxes</span>
-            <span>$0</span>
-          </div>
-        </div>
-        <Separator />
-        <div className="text-gray-800 font-semibold flex justify-between">
-          <span>Total</span>
-          <span>$200</span>
-        </div>
+        <SubTotal />
       </CardContent>
 
       <CardFooter>
