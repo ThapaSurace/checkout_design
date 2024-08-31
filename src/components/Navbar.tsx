1cd01702becae3626100b8f42a60c42ae690af03
ThapaSurace <zorosenpai456@gmail.com>
2024-08-31 12:43:03 +0545

    add navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,11 @@
-import { ShoppingBag } from "lucide-react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
-import { Input } from "./ui/input";
 
 const Navbar = () => {
   return (
     <div className="py-4 border-b px-4">
       <MaxWidthWrapper>
-        <div className="relative flex justify-between items-center">
-          <Input type="text" className="w-[300px]" placeholder="search..." />
-          <div className="absolute left-1/2 transform -translate-x-1/2 font-bold text-2xl text-slate-900">
-            Store
-          </div>
-          <div className="flex items-center text-gray-600">
-            <ShoppingBag size={20} />
-            <span className="text-lg">(2)</span>
-          </div>
+        <div className="flex gap-1 text-2xl items-center text-slate-950 font-bold">
+          Store
         </div>
       </MaxWidthWrapper>
     </div>
