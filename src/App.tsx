22f649f087008e18fef9d12a997d08d161557978
ThapaSurace <zorosenpai456@gmail.com>
2024-08-31 08:34:28 +0545

    add checkout route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
+import Checkout from "./pages/Checkout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Cart />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
   },
 ]);
 
