5e6386fde331c0a7b878c6e3d6a00aaa78160e91
ThapaSurace <zorosenpai456@gmail.com>
2024-09-01 15:25:36 +0545

    toaster component implemented

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./pages/Cart";
 import Navbar from "./components/Navbar";
 import Checkout from "./pages/Checkout";
+import { Toaster } from "./components/ui/toaster";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const App = () => {
     <>
       <Navbar />
       <RouterProvider router={router} />
+      <Toaster />
     </>
   );
 };
